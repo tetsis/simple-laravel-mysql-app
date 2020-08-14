b0e2bccdc5f777451c0d7a2a1f24183a458e7c34
tetsis <hitoshi.zin.nakao@gmail.com>
2020-08-14 18:14:32 +0900

    [add] Save data to model

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Hello;
 
 class HelloController extends Controller
 {
     //
     public function index()
     {
-        return view('hello');
+        $items = Hello::all();
+
+        return view('hello', compact('items'));
+    }
+
+    public function post(Request $req)
+    {
+        $hello = new Hello;
+        $hello->save();
+
+        return redirect('/hello');
     }
 }
--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -1,4 +1,13 @@
 <!DOCTYPE html>
 <html>
 hello
+<form method="POST">
+    <ul>
+        @foreach ($items as $item)
+        <li>{{ $item->created_at }}</li>
+        @endforeach
+    </ul>
+    {{ csrf_field() }}
+    <div><input type="submit" name="save" value="Save"></div>
+</form>
 </html>
